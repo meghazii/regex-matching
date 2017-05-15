8c86cda2c4f181da0d3c7b1981f64c31f232f4aa
imran <meghazi.imran@gmail.com>
2017-05-15 17:50:09 -0400

    doing stuff

--- main.c
+++ main.c
@@ -1,38 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
 #include "main.h"
-#include "../../emacs/src/lisp.h"
+
+int cc = 257; //nombre de charclass
+int nNodes = 4; //nombre de noeud/état dans l'automate
+Lisp_Object array;
 
 int main(){
+  
+  //récupere tableau lisp de taille : cc * nNodes
+  //soit x % (cc * nNodes) + cTable = une classe de caractere
 
-  
-  
+  int currentState;
+  char** s = "abba";
+ 
+  for(; *s; s++)
+    {
+      currentState = f(*s, currentState);
+      if(currentState == 0 || currentState == -1) break;
+    }
+
+  if(currentState == 0) printf("Succes\n");
+  else printf("Fail\n");
   return 0;
 }
 
-void addState()
+  
+int DFA(char c, int currentState)
 {
+  if(AREF(array, CHAR_TABLE(c)) != Qnil)
+    {
+      if(INTEGERP(AREF(array, CHAR_TABLE(c))))
+	{
+	  return(AREF(array, CHAR_TABLE(c)));
+	}
+      else
+	{
+	  return -1;
+	}
+    }
 }
 
-void start()
-{
-}
-
-void match()
-{
-}
-
-_Bool is_DFA(Vector x)
-{
-  if(INTEGERP(x)) return true;
-}
-
-void function()
+int
+DFA_Mark(Lisp_Object cMap, int ncc, Lisp_Object c, int cState, Lisp_Object array)
 {
   
+  Lisp_Object tmp = AREF (array, cState*ncc + cc1);
+  if (INTEGERP (tmp))
+    {
+      Lisp_Object cc = Faref (cMap, c);
+      if (! INTEGERP(cc))
+	{
+	  error ("plop 2");
+	}
+      int cc1 = XINT(cc);
+      if (cc1 >= ncc)
+	{
+	  error ("plop 3");
+	}
+      return XINT(tmp);
+    }
+  else if (CONSP(tmp))
+    {
+      save (XCAR (XCDR (tmp)));
+      DFA_Mark (XCAR (XCDR (XCDR (tmp))));
+    }
+  else
+    {
+      error ("plop");
+    }
 }
-
-
-
--- main.h
+++ main.h
@@ -1,11 +1,6 @@
 #ifndef REGEXP_H
 #define REGEXP_H
 
-struct Nodes
-{
-  
-};
-
 struct State
 {
   int c;
@@ -20,8 +15,12 @@ struct List
   int n;
 };
 
-_Bool is_DFA(Vector x);
+void addState(struct List *l, struct State *s);
 
+struct List* startList(struct State *s, struct List *l);
 
+void match(struct List *cList, int c, struct List *nList);
+
+int is_matched(struct List *l);
 
 #endif
