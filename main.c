f5e8892766de47e3e77af6e30a17192a9c0799bd
imran <meghazi.imran@gmail.com>
2017-05-17 16:17:22 -0400

    cleaning

--- main.c
+++ main.c
@@ -2,71 +2,93 @@
 #include <stdlib.h>
 #include "main.h"
 
-int cc = 257; //nombre de charclass
-int nNodes = 4; //nombre de noeud/état dans l'automate
-Lisp_Object array;
 
 int main(){
-  
-  //récupere tableau lisp de taille : cc * nNodes
-  //soit x % (cc * nNodes) + cTable = une classe de caractere
 
-  int currentState;
-  char** s = "abba";
- 
-  for(; *s; s++)
-    {
-      currentState = f(*s, currentState);
-      if(currentState == 0 || currentState == -1) break;
-    }
-
-  if(currentState == 0) printf("Succes\n");
-  else printf("Fail\n");
   return 0;
 }
 
-  
-int DFA(char c, int currentState)
+int NFA_matching ()
 {
-  if(AREF(array, CHAR_TABLE(c)) != Qnil)
-    {
-      if(INTEGERP(AREF(array, CHAR_TABLE(c))))
-	{
-	  return(AREF(array, CHAR_TABLE(c)));
-	}
-      else
-	{
-	  return -1;
-	}
-    }
+  
 }
 
 int
-DFA_Mark(Lisp_Object cMap, int ncc, Lisp_Object c, int cState, Lisp_Object array)
+DFA_Mark (Lisp_Object cMap, int ncc, Lisp_Object c, int cState, Lisp_Object array)
 {
-  
+  Lisp_Object cc = Faref (cMap, c);
+  if (! INTEGERP(cc))
+    {
+      error ("plop 2");
+    }
+  int cc1 = XINT(cc);
   Lisp_Object tmp = AREF (array, cState*ncc + cc1);
+  
+  if (cc1 >= ncc)
+    {
+      error ("plop 3");
+    }
   if (INTEGERP (tmp))
     {
-      Lisp_Object cc = Faref (cMap, c);
-      if (! INTEGERP(cc))
-	{
-	  error ("plop 2");
-	}
-      int cc1 = XINT(cc);
-      if (cc1 >= ncc)
-	{
-	  error ("plop 3");
-	}
-      return XINT(tmp);
+      return XINT (tmp);
     }
   else if (CONSP(tmp))
     {
-      save (XCAR (XCDR (tmp)));
-      DFA_Mark (XCAR (XCDR (XCDR (tmp))));
+      if (XCAR (tmp) == Qmark)
+	{
+	  save (XCAR (XCDR (tmp)));
+	  DFA_Mark (cMap, ncc, c, cState, array);
+	}
     }
   else
     {
       error ("plop");
     }
+}
+
+int
+DFA_matching (Lisp_Object cMap, int ncc, Lisp_Object c, int cState, Lisp_Object array)
+{
+  Lisp_Object cc = Faref (cMap, c);
+  if (! INTEGERP(cc))
+    {
+      error ("plop 2");
+    }
+  int cc1 = XINT(cc);
+  Lisp_Object tmp = AREF (array, cState*ncc + cc1);
+  
+  if (cc1 >= ncc)
+    {
+      error ("plop 3");
+    }
+  if (INTEGERP (tmp))
+    {
+      return XINT(tmp);
+    }
+  else
+    {
+      error ("plop");
+    }
+}
+
+Lisp_Object
+match(Lisp_Object array, Lisp_Object cMap, Lisp_Object string, unsigned int ncc, unsigned int nState)
+{
+  int cState = 0;
+  eassert (STRINGP (string));
+  char *s = malloc(4 * sizeof(char));
+  *s = XSTRING (string);
+  for (; *s; s++)
+    {
+      cState = DFA_matching (cMap, ncc, string, cState, array);
+      if (cState == 0 || cState == -1) break;
+    }
+  if (cState == 0) return 1;
+  else return 0;
+}
+
+DEFUN ("DFA_matching", FDFA_matching, SDFA_matching, 5, 5, 0, doc : )
+  (Lisp_Object array, Lisp_Object cMap, Lisp_Object string, unsigned int ncc, unsigned int nState)
+{
+  return match(array, cMap, string, ncc, nState);
 }
